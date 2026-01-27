ee9284b4853eab3a6a8ebcd730310f5e55dbcefe
Fabian Hiller <hillerfabian11@gmail.com>
2026-01-26 22:17:16 -0500

    Address GitHub copilot review comments

--- frameworks/solid/eslint.config.js
+++ frameworks/solid/eslint.config.js
@@ -5,15 +5,19 @@ import {
 } from '@formisch/eslint-config';
 import solidLint from 'eslint-plugin-solid/configs/typescript';
 
+const sourceConfig = createSourceConfig({
+  tsconfigRootDir: import.meta.dirname,
+});
+
 export default tseslint.config(
   { ignores: ['eslint.config.js'] },
   ...baseConfigs,
   {
-    ...createSourceConfig({ tsconfigRootDir: import.meta.dirname }),
-    plugins: { ...solidLint.plugins, ...createSourceConfig().plugins },
+    ...sourceConfig,
+    plugins: { ...solidLint.plugins, ...sourceConfig.plugins },
     rules: {
       ...solidLint.rules,
-      ...createSourceConfig().rules,
+      ...sourceConfig.rules,
     },
   }
 );
